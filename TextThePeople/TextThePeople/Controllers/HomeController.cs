18753be0457ffaee6502c16a7334e12a3d7e45f6
LKordecki <lkordecki@emphasys-software.com>
2013-09-27 10:43:54 -0400

    Changes to the views

--- TextThePeople/TextThePeople/Controllers/HomeController.cs
+++ TextThePeople/TextThePeople/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TextThePeople.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Contact your tennants or landlords through phone based text messaging system.";
 
             return View();
         }
