3bd743a94d0cbf18705a84b21c3c6b7e6a16d106
jgonzalezEmphasys <jgonzalezs@emphasys-software.com>
2013-09-27 20:35:57 -0400

    Updated the message controller to do some validation and send errors if there's problems

--- TextThePeople/TextThePeople/Controllers/MessageController.cs
+++ TextThePeople/TextThePeople/Controllers/MessageController.cs
@@ -3,7 +3,11 @@ using System.Net.Http;
 using System.Text;
 using System.Web.Http;
 using TextThePeople.DAL;
+using System.Linq;
 using Twilio;
+using TextThePeople.Models;
+using System.Net;
+using System;
 
 namespace TextThePeople.Controllers
 {
@@ -23,32 +27,54 @@ namespace TextThePeople.Controllers
         }
 
         // POST api/sendmessage?from,to,message
-        public void Post(string from, string to, string message) 
-        {
-            from = from ?? defaultNumber;
-
-            var accountSid = "AC3c18b86d6c4941dc9ea2c15f090f6d03";
-            var authToken = "3a401907393636c13902e6ef930a0e76";
-            var twilioClient = new TwilioRestClient(accountSid, authToken);
-
-            twilioClient.SendMessage(from, Normalize(to), message, new string[0]);
-        }
-
         [HttpPost]
-        // POST api/groupmessage?from,recipients,message
-        public void Post(string from, IEnumerable<string> recipients, string message) 
+        public HttpResponseMessage Post(string from, string to, string osentityId,string message) 
         {
             from = from ?? defaultNumber;
-
+            Persons person;
+            try
+            {
+                if (osentityId != null)
+                    person = db.Persons.ToList().Where(x => x.OSEntityPK.ToString() == osentityId).First();
+                else
+                    person = db.Persons.ToList().Where(x => x.PhoneNumber == to).First();
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
             var accountSid = "AC3c18b86d6c4941dc9ea2c15f090f6d03";
             var authToken = "3a401907393636c13902e6ef930a0e76";
             var twilioClient = new TwilioRestClient(accountSid, authToken);
 
-            foreach (var number in recipients)
+            try
             {
-                twilioClient.SendMessage(from, Normalize(number), message, new string[0]);
+                twilioClient.SendMessage(from, Normalize(to), message, new string[0]);
             }
+            catch
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
+
+
+
+        //[HttpPost]
+        //// POST api/groupmessage?from,recipients,message
+        //public void Post(string from, IEnumerable<string> recipients, string message) 
+        //{
+        //    from = from ?? defaultNumber;
+
+        //    var accountSid = "AC3c18b86d6c4941dc9ea2c15f090f6d03";
+        //    var authToken = "3a401907393636c13902e6ef930a0e76";
+        //    var twilioClient = new TwilioRestClient(accountSid, authToken);
+
+        //    foreach (var number in recipients)
+        //    {
+        //        twilioClient.SendMessage(from, Normalize(number), message, new string[0]);
+        //    }
+        //}
 
         private string Normalize(string to)
         {
@@ -56,7 +82,7 @@ namespace TextThePeople.Controllers
             for (int i = 0; i < to.Length; i++)
                 if (char.IsNumber(to[i])) sb.Append(to[i]);
 
-            if (sb.Length == 10) return sb.ToString();
+            if (sb.Length == 11) return sb.ToString();
             else return ""; // invalid number
 
         }
