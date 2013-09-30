238414c19c788b44e25a76866fcfcd60b4b4d408
Adrian Carballo <acarballo@emphasys-software.com>
2013-09-30 10:39:34 -0400

    updates to the config files

--- TextThePeople/TextThePeople/Global.asax.cs
+++ TextThePeople/TextThePeople/Global.asax.cs
@@ -24,5 +24,23 @@ namespace TextThePeople
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
         }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id =
+                        UrlParameter.Optional
+                }
+            );
+        }
     }
 }
